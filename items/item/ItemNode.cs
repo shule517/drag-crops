a50790f99fc1c21b1d76d77b975b167f992c9eae
shule517 <shule517@gmail.com>
2025-08-27 16:32:51 +0900

    ItemNodeにrename

--- items/item/ItemNode.cs
+++ items/item/ItemNode.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public partial class Item : CharacterBody2D
+public partial class ItemNode : CharacterBody2D
 {
     [Export] public AudioStream GetItemAudio;
     [Export] public AudioStream LevelUpAudio;
