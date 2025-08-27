af108d61ba6256b3b1498905ff6f726da83ce54c
shule517 <shule517@gmail.com>
2025-08-27 16:23:05 +0900

    炭鉱の壁を描画

--- fields/Field.cs
+++ fields/Field.cs
@@ -7,22 +7,27 @@ public partial class Field : Node2D
     private PackedScene _treeScene = GD.Load<PackedScene>("res://objects/tree/tree.tscn");
     private PackedScene _ironOreScene = GD.Load<PackedScene>("res://objects/iron_ore/iron_ore.tscn");
     private List<Vector2> _objectPositions = new List<Vector2>();
+    private TileMapLayer _wallTileMapLayer;
 
     public override void _Ready()
     {
+        _wallTileMapLayer = GetNode<TileMapLayer>("WallTileMapLayer");
+
+        // 木の追加
         30.Times((x) =>
         {
             30.Times((y) =>
             {
-                Spawn(GD.RandRange(0, 1000), GD.RandRange(0, 1000), _treeScene);
+                Spawn(GD.RandRange(0, 1000), GD.RandRange(5, 1000), _treeScene);
             });
         });
 
+        // 鉱石の追加
         30.Times((x) =>
         {
             30.Times((y) =>
             {
-                Spawn(GD.RandRange(0, 1000), GD.RandRange(0, -1000), _ironOreScene);
+                Spawn(GD.RandRange(0, 1000), GD.RandRange(-5, -1000), _ironOreScene);
             });
         });
     }
@@ -42,5 +47,16 @@ public partial class Field : Node2D
         var objectNode = objectScene.Instantiate<Node2D>();
         objectNode.GlobalPosition = new Vector2(x, y);
         CallDeferred("add_child", objectNode);;
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        if (@event is InputEventMouseMotion mouseEvent)
+        {
+            Vector2 localPos = GetGlobalMousePosition();
+
+            GD.Print(localPos.Y);
+            _wallTileMapLayer.Visible = 0 < localPos.Y;
+        }
     }
 }
