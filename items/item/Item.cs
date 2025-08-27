48cbe5ef8c486cec9a03b15e75e7a92a114ee3e0
shule517 <shule517@gmail.com>
2025-08-27 16:55:40 +0900

    Itemクラスを追加した

---
+++ items/item/Item.cs
@@ -0,0 +1,6 @@
+using dragcrops.items.item;
+
+public class Item(ItemType itemType)
+{
+    ItemType ItemType { get; } = itemType;
+}
---
+++ items/item/ItemType.cs
@@ -0,0 +1,19 @@
+namespace dragcrops.items.item;
+
+public enum ItemType
+{
+    木材,
+    堅い木,
+    樹液,
+    たいまつ,
+    肥料,
+    石,
+    石炭,
+    鉄鉱石,
+    銀鉱石,
+    金鉱石,
+    鉄インゴット,
+    銀インゴット,
+    金インゴット,
+    溶鉱炉
+}
