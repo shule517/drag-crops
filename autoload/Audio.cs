185088aeb8b0effd10a180dddefc905ad0e600d7
shule517 <shule517@gmail.com>
2025-09-03 21:57:47 +0900

    不要なコードを削除した

--- autoload/Audio.cs
+++ autoload/Audio.cs
@@ -4,6 +4,20 @@ using Godot;
 [AutoLoad]
 public partial class Audio : Node
 {
+    public void Play(AudioStream audioStream, double pitchScale = 1.0f, float volumeDb = 0.0f)
+    {
+        var audioPlayer = new AudioStreamPlayer2D();
+        audioPlayer.Stream = audioStream;
+        audioPlayer.PitchScale = (float)pitchScale;
+        audioPlayer.VolumeDb = volumeDb;
+        audioPlayer.Autoplay = true;
+        GetTree().CurrentScene.AddChild(audioPlayer);
+        // player.global_position = node.global_position # 再生位置を設定
+
+        // 再生終了後に削除する
+        audioPlayer.Finished += () => audioPlayer.QueueFree();
+    }
+
     // private static Audio Instance { get; } = SceneTree.Root.GetNode<Audio>("/root/AutoLoad/Audio");
     // private static SceneTree SceneTree => (SceneTree)Engine.GetMainLoop();
 
@@ -23,33 +37,4 @@ public partial class Audio : Node
     // _Readyより前に実行されると落ちる
 
     // TODO: ↑どっちもTreeから検索してるから一緒では？
-
-    // public static void PlaySound(AudioStream audioStream, double pitchScale = 1.0f, float volumeDb = 0.0f)
-    // {
-    //     var audioPlayer = new AudioStreamPlayer2D();
-    //     audioPlayer.Stream = audioStream;
-    //     audioPlayer.PitchScale = (float)pitchScale;
-    //     audioPlayer.VolumeDb = volumeDb;
-    //     audioPlayer.Autoplay = true;
-    //     Instance.GetTree().CurrentScene.AddChild(audioPlayer);
-    //     // player.global_position = node.global_position # 再生位置を設定
-    //
-    //     // 再生終了後に削除する
-    //     audioPlayer.Finished += () => audioPlayer.QueueFree();
-    // }
-
-    public void Play(AudioStream audioStream, double pitchScale = 1.0f, float volumeDb = 0.0f)
-    {
-        var audioPlayer = new AudioStreamPlayer2D();
-        audioPlayer.Stream = audioStream;
-        audioPlayer.PitchScale = (float)pitchScale;
-        audioPlayer.VolumeDb = volumeDb;
-        audioPlayer.Autoplay = true;
-        GetTree().CurrentScene.AddChild(audioPlayer);
-        // player.global_position = node.global_position # 再生位置を設定
-
-        // 再生終了後に削除する
-        audioPlayer.Finished += () => audioPlayer.QueueFree();
-    }
-
 }
